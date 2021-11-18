84fc4670e99742010d77564bd939279852141f9f
Тюрюмов Сергей <92250133+styuryumov@users.noreply.github.com>
2021-11-18 20:59:23 +0600

    Вторая домашняя работа
    Добавил test-case в class AdminTest, в class TestBase добавил проверку инициализации драйвера, а также метод проверяющий возвращение не нулевого списка.

--- src/test/java/ru/stqa/selenium/AdminTest.java
+++ src/test/java/ru/stqa/selenium/AdminTest.java
@@ -1,8 +1,13 @@
 package ru.stqa.selenium;
 
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+import java.util.List;
 
 public class AdminTest extends TestBase {
 
@@ -16,7 +21,32 @@ public class AdminTest extends TestBase {
         driver.findElement(By.name("username")).sendKeys(prop.getProperty("login"));
         driver.findElement(By.name("password")).sendKeys(prop.getProperty("password"));
         driver.findElement(By.name("login")).click();
-        wait.until(driver -> driver.findElements(By.id("body-wrapper")).size() > 0);
+        wait.until(driver -> driver.findElements(By.cssSelector("div#body-wrapper")).size() > 0);
+        }
+
+    @Test
+    public void testAdminPanel() {
+        List<WebElement> list = driver.findElements(By.cssSelector("li#app-"));
+        int iterations = list.size();
+
+        for (int i = 0; i < iterations; i++) {
+             list = driver.findElements(By.cssSelector("li#app-"));
+             list.get(i).click();
+             wait.until(presenceOfElementLocated(By.cssSelector("h1")));
+             if (areElementsPresent(driver, By.cssSelector("ul.docs [id*=doc-]"))) {
+                 List<WebElement> nestedElements = driver.findElements(By.cssSelector("ul.docs [id*=doc-]"));
+                 for (int j = 0; j < nestedElements.size(); j++) {
+                     nestedElements = driver.findElements(By.cssSelector("ul.docs [id*=doc-]"));
+                     nestedElements.get(j).click();
+                     wait.until(presenceOfElementLocated(By.cssSelector("h1")));
+                 }
+             }
+             else {
+                 continue;
+             }
+
+        }
+
     }
 
 }
--- src/test/java/ru/stqa/selenium/TestBase.java
+++ src/test/java/ru/stqa/selenium/TestBase.java
@@ -2,31 +2,43 @@ package ru.stqa.selenium;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 
 import java.io.IOException;
 import java.util.Properties;
 
 public class TestBase {
 
-    public WebDriver driver;
+    public static WebDriver driver;
     public Properties prop;
-    public WebDriverWait wait;
+    public static WebDriverWait wait;
+
+    boolean areElementsPresent(WebDriver driver, By locator) {
+        return driver.findElements(locator).size() > 0;
+    }
 
     @Before
     public void start() throws IOException {
         prop = new Properties();
         prop.load(TestBase.class.getResourceAsStream("/testData.properties"));
-        driver = new FirefoxDriver();
+
+        if (driver != null) {
+            return;
+        }
+        ChromeOptions options =new ChromeOptions();
+        options.addArguments("start-fullscreen");
+        driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, 10);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {driver.quit(); driver = null;}));
     }
 
     @After
     public void stop() {
-        driver.quit();
-        driver = null;
+        //driver.quit();
+        //driver = null;
     }
 }
