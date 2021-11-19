832126f3913bd92ef35820d0d4b765b9fb1dacd3
Тюрюмов Сергей <92250133+styuryumov@users.noreply.github.com>
2021-11-19 13:21:14 +0600

    Вторая домашняя работа. Упражнение 8

---
+++ src/test/java/ru/stqa/selenium/HomePage.java
@@ -0,0 +1,27 @@
+package ru.stqa.selenium;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class HomePage extends TestBase {
+
+    @Before
+    public void enterTheLink() {
+        driver.get((prop.getProperty("baseUrl")) + "/" + (prop.getProperty("langu") + "/"));
+    }
+
+    @Test
+    public void checkForStickersTest() {
+        List<WebElement> list = driver.findElements(By.cssSelector("div#main [class=link]"));
+
+        for (int i = 0; i < list.size(); i++) {
+            WebElement element = list.get(i);
+            Assert.assertTrue(element.findElements(By.cssSelector("[class^=sticker]")).size() == 1);
+        }
+    }
+}
