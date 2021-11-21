a3cb29003512111119fa271bf66dc2ae0aa74286
Тюрюмов Сергей <92250133+styuryumov@users.noreply.github.com>
2021-11-21 22:26:30 +0600

    Третье домашнее задание ST QA. Упражнение 10
    добавил в TestBase возможность выбора браузера.
    в HomePage реализовал упражнение 10

--- src/test/java/ru/stqa/selenium/HomePage.java
+++ src/test/java/ru/stqa/selenium/HomePage.java
@@ -5,7 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
 
+
+import java.awt.*;
 import java.util.List;
 
 public class HomePage extends TestBase {
@@ -23,5 +26,51 @@ public class HomePage extends TestBase {
             WebElement element = list.get(i);
             Assert.assertTrue(element.findElements(By.cssSelector("div.sticker")).size() == 1);
         }
+    }
+
+    @Test
+    public void matchProductsPage() {
+        String nameHomePage = driver.findElement(By.cssSelector("div#box-campaigns div.name")).getText();
+        String regularPriceHomePage = driver.findElement(By.cssSelector("div#box-campaigns s.regular-price")).getText();
+        String campaignPriceHomePage = driver.findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getText();
+        String strikeRegularPriceHomePage = driver.findElement(By.cssSelector("div#box-campaigns s.regular-price")).getTagName();
+        String strongCampaignPriceHomePage = driver.findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getTagName();
+        String sSizeRegularPriceHomePage = driver.findElement(By.cssSelector("div#box-campaigns s.regular-price")).getCssValue("font-size");
+        String sSizeCampaignPriceHomePage = driver.findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getCssValue("font-size");
+        int sizeRegularPriceHomePage = size(sSizeRegularPriceHomePage);
+        int sizeCampaignPriceHomePage = size(sSizeCampaignPriceHomePage);
+        boolean bSizeHomePage = sizeComparison(sizeRegularPriceHomePage, sizeCampaignPriceHomePage);
+        Assert.assertEquals(true, bSizeHomePage);
+        String colorRegularPriceHomePage = driver.findElement(By.cssSelector("div#box-campaigns s.regular-price")).getCssValue("color");
+        String colorCampaignPriceHomePage = driver.findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getCssValue("color");
+        Color greyColorHomePage = Color.fromString(colorRegularPriceHomePage);
+        Color redColorHomePage = Color.fromString(colorCampaignPriceHomePage);
+        Assert.assertEquals(true, color(greyColorHomePage));
+        Assert.assertEquals(true, color(redColorHomePage));
+        driver.findElement(By.cssSelector("div#box-campaigns li.product")).click();
+        String nameProductPage = driver.findElement(By.cssSelector("div#box-product h1")).getText();
+        String regularPriceProductPage = driver.findElement(By.cssSelector("div#box-product s.regular-price")).getText();
+        String campaignPriceProductPage = driver.findElement(By.cssSelector("div#box-product strong.campaign-price")).getText();
+        String strikeRegularPriceProductPage = driver.findElement(By.cssSelector("div#box-product s.regular-price")).getTagName();
+        String strongCampaignPriceProductPage = driver.findElement(By.cssSelector("div#box-product strong.campaign-price")).getTagName();
+        String sSizeRegularPriceProductPage = driver.findElement(By.cssSelector("div#box-product s.regular-price")).getCssValue("font-size");
+        String sSizeCampaignPriceProductPage = driver.findElement(By.cssSelector("div#box-product strong.campaign-price")).getCssValue("font-size");
+        int sizeRegularPriceProductPage = size(sSizeRegularPriceProductPage);
+        int sizeCampaignPriceProductPage = size(sSizeCampaignPriceProductPage);
+        boolean bSizeProductPage = sizeComparison(sizeRegularPriceProductPage, sizeCampaignPriceProductPage);
+        Assert.assertEquals(true, bSizeProductPage);
+        String colorRegularPriceProductPage = driver.findElement(By.cssSelector("div#box-product s.regular-price")).getCssValue("color");
+        String colorCampaignPriceProductPage = driver.findElement(By.cssSelector("div#box-product strong.campaign-price")).getCssValue("color");
+        Color greyColorProductPage = Color.fromString(colorRegularPriceHomePage);
+        Color redColorProductPage = Color.fromString(colorCampaignPriceHomePage);
+        Assert.assertEquals(true, color(greyColorProductPage));
+        Assert.assertEquals(true, color(redColorProductPage));
+        driver.navigate().back();
+        Assert.assertEquals(nameHomePage, nameProductPage);
+        Assert.assertEquals(regularPriceHomePage, regularPriceProductPage);
+        Assert.assertEquals(campaignPriceHomePage, campaignPriceProductPage);
+        Assert.assertEquals(strikeRegularPriceHomePage, strikeRegularPriceProductPage);
+        Assert.assertEquals(strongCampaignPriceHomePage, strongCampaignPriceProductPage);
+
     }
 }
--- src/test/java/ru/stqa/selenium/TestBase.java
+++ src/test/java/ru/stqa/selenium/TestBase.java
@@ -6,13 +6,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import org.openqa.selenium.support.Color;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Properties;
 
 public class TestBase {
-
     public static WebDriver driver;
     public Properties prop;
     public static WebDriverWait wait;
@@ -20,6 +26,36 @@ public class TestBase {
     boolean areElementsPresent(WebDriver driver, By locator) {
         return driver.findElements(locator).size() > 0;
     }
+
+    public int size(String string) {
+        String[] strArr = string.split("");
+        if (Objects.equals(strArr[1], ".")) {
+            strArr[1] = strArr[2];
+        }
+        return Integer.parseInt(strArr[0] + strArr[1]);
+    }
+
+    boolean sizeComparison(int size1, int size2) {
+        return size1 < size2;
+    }
+
+    boolean color (Color color) {
+        int red = color.getColor().getRed();
+        int green = color.getColor().getGreen();
+        int blue = color.getColor().getBlue();
+
+        if ((red == green) && (green == blue)) {
+            return true;
+        }
+        else if ((red != green) && (green == blue)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
 
     @Before
     public void start() throws IOException {
@@ -29,11 +65,26 @@ public class TestBase {
         if (driver != null) {
             return;
         }
-        ChromeOptions options =new ChromeOptions();
-        options.addArguments("start-fullscreen");
-        driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, 10);
+        if ("chrome".equals(prop.getProperty("browser"))) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-fullscreen");
+            driver = new ChromeDriver(options);
+            wait = new WebDriverWait(driver, 10);
+        }
+        else if ("firefox".equals(prop.getProperty("browser"))) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("start-fullscreen");
+            driver = new FirefoxDriver(options);
+            wait = new WebDriverWait(driver, 10);
+        }
+        else if ("ie".equals(prop.getProperty("browser"))) {
+            InternetExplorerOptions options = new InternetExplorerOptions();
+            options.addCommandSwitches("start-fullscreen");
+            driver = new InternetExplorerDriver(options);
+            wait = new WebDriverWait(driver, 10);
+        }
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {driver.quit(); driver = null;}));
+
     }
 
     @After
