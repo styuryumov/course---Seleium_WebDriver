c896fddaade8f84ab20d17de44f8175a8b8aa695
Тюрюмов Сергей <92250133+styuryumov@users.noreply.github.com>
2021-11-28 00:10:42 +0600

    Revert "Четвертое домашнее задание. Упражнение 12. Сделал более общий метод проверки добавления элемента"
    This reverts commit 2f964d3898e270ddcf94fa7f79f804375015481c.

--- src/test/java/ru/stqa/selenium/AdminTest.java
+++ src/test/java/ru/stqa/selenium/AdminTest.java
@@ -130,8 +130,7 @@ public class AdminTest extends TestBase {
     public void creationNewProduct() throws InterruptedException {
         List<WebElement> list = driver.findElements(By.cssSelector("li#app-"));
         list.get(1).click();
-        Assert.assertEquals("Catalog", driver.findElement(By.cssSelector("td#content h1")).getText());
-        List<WebElement> tableBefore = driver.findElements(By.cssSelector("table.dataTable tr.row"));
+        Assert.assertEquals(" Catalog", driver.findElement(By.cssSelector("td#content h1")).getAttribute("outerText"));
         List<WebElement> addNew = driver.findElements(By.cssSelector("td#content a.button"));
         addNew.get(1).click();
         List<WebElement> tabs = driver.findElements(By.cssSelector("div.tabs li a"));
@@ -181,8 +180,10 @@ public class AdminTest extends TestBase {
         driver.findElement(By.cssSelector("span.button-set [name=save]")).click();
         // verification of successful product addition
         Thread.sleep(1500);
-        List<WebElement> tableAfter = driver.findElements(By.cssSelector("table.dataTable tr.row"));
-        Assert.assertTrue(tableAfter.size() > tableBefore.size());
+        driver.findElement(By.cssSelector("td#content [name=query]")).sendKeys("Bat-Duck" + Keys.ENTER);
+        list.clear();
+        list = driver.findElements(By.cssSelector("table.dataTable a"));
+        Assert.assertEquals(" Bat-Duck", list.get(0).getAttribute("textContent"));
     }
 }
 
