3d29d88feaf231b85d45fd787c2068a3b1b83329
Тюрюмов Сергей <92250133+styuryumov@users.noreply.github.com>
2021-11-29 20:26:05 +0600

    Шестое домашнее задание. Упражнение 14

--- src/test/java/ru/stqa/selenium/AdminTest.java
+++ src/test/java/ru/stqa/selenium/AdminTest.java
@@ -1,11 +1,14 @@
 package ru.stqa.selenium;
 
+import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 
@@ -18,6 +21,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
+import java.util.Set;
 
 public class AdminTest extends TestBase {
 
@@ -185,6 +189,35 @@ public class AdminTest extends TestBase {
         wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("table.dataTable a"), "Bat-Duck"));
         list = driver.findElements(By.cssSelector("table.dataTable a"));
         Assert.assertEquals("Bat-Duck", list.get(0).getText());
+    }
+
+    @Test
+    public void testExternalLinkByCountries() {
+        List<WebElement> list = driver.findElements(By.cssSelector("li#app-"));
+        list.get(2).click();
+        driver.findElement(By.cssSelector("td#content a.button")).click();
+        String activeWindow = driver.getWindowHandle();
+        Set<String> oldWindows = driver.getWindowHandles();
+        List<WebElement> externalLinks = driver.findElements(By.cssSelector("td#content i.fa.fa-external-link"));
+        int iterations = externalLinks.size();
+        for (int i = 0; i < iterations; i++) {
+            externalLinks.get(i).click();
+            String newWindow = wait.until(new ExpectedCondition<String>() {
+                @NullableDecl
+                @Override
+                public String apply(@NullableDecl WebDriver driver) {
+                    Set<String> newWindows = driver.getWindowHandles();
+                    newWindows.removeAll(oldWindows);
+                    return newWindows.size() > 0 ? newWindows.iterator().next() : null;
+                }
+            });
+            driver.switchTo().window(newWindow);
+            driver.close();
+            driver.switchTo().window(activeWindow);
+            Assert.assertEquals("Add New Country", driver.findElement(By.cssSelector("td#content h1")).getText());
+        }
+        driver.findElement(By.cssSelector("span.button-set [name=cancel]")).click();
+        Assert.assertEquals("Countries", driver.findElement(By.cssSelector("td#content h1")).getText());
     }
 }
 
